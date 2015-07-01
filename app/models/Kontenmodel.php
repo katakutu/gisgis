30140e3b5fda36eb1ec4680f6eac0c0510c27b51
Pomirleanu Florentin Cristinel <pomirleanu.florentin@gmail.com>
2015-07-01 11:28:53 +0300

    try this

--- app/models/Kontenmodel.php
+++ app/models/Kontenmodel.php
@@ -1,5 +1,11 @@
 <?php
-class Kontenmodel extends Eloquent
-{
-	public $table = 'konten';
-}
+    namespace App\models;
+
+    use Illuminate\Database\Eloquent\Model;
+
+    class KontenModel extends Model {
+
+        protected $table = 'konten';
+
+        protected $fillable = ['is_admin', 'views', 'date', 'judul', 'gambar', 'slug', 'content', 'summary', 'kolom_informasi', 'urutan'];
+    }
